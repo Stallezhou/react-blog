6b638f72c9e8528658ebbf156aa24ed5457142b2
stallezhou <2272133935@qq.com>
2020-12-21 14:58:56 +0800

    新增首页list渲染

---
+++ blog/pages/content.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Header from '../components/Header/index'
+export default function Content() {
+  return (<div>
+    <Head>
+     <title>Content</title>
+    </Head>
+    <Header></Header>
+  </div>
+  )
+}
---
+++ blog/pages/list.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Header from '../components/Header/index'
+export default function List() {
+  return (<div>
+    <Head>
+     <title>list</title>
+    </Head>
+    <Header></Header>
+  </div>
+  )
+}
