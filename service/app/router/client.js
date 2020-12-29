88e9ffd0d3fbe36f7071e3402c694caf58b478f3
stallezhou <2272133935@qq.com>
2020-12-29 11:28:42 +0800

    前后台服务目录结构分离

--- service/app/router/client.js
+++ service/app/router/client.js
@@ -1,8 +1,10 @@
+
 'use strict';
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  require('./router/client')(app);
+  const { router, controller } = app;
+  router.get('/client/index', controller.client.home.index);
 };
